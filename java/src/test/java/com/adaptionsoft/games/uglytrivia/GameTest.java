c6a17e6f266d2fa1ef23969f25c4980a24f89655
Samir Talwar <samir@noodlesandwich.com>
2012-06-20 21:46:54 +0100

    Tested multiple players.

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -30,4 +30,21 @@ public class GameTest {
         game.add("Bob");
         assertThat(outputStream.toString(), is("Bob was added\nThey are player number 1\n"));
     }
+
+    @Test public void
+    adds_multiple_players() {
+        Game game = new Game();
+        game.add("Julian");
+        game.add("Dick");
+        game.add("George");
+        game.add("Anne");
+        game.add("Timmy");
+        assertThat(outputStream.toString(), is(
+            "Julian was added\nThey are player number 1\n"
+          + "Dick was added\nThey are player number 2\n"
+          + "George was added\nThey are player number 3\n"
+          + "Anne was added\nThey are player number 4\n"
+          + "Timmy was added\nThey are player number 5\n"
+        ));
+    }
 }
