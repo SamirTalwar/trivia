4584aae621062503fc2e5eab0330ef7260fc1206
Samir Talwar <samir@noodlesandwich.com>
2012-06-20 21:57:47 +0100

    Wrote a test for correctly answered questions.

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -2,20 +2,23 @@ package com.adaptionsoft.games.uglytrivia;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.contains;
 
 public class GameTest {
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    private final PrintStream out = new PrintStream(outputStream);
     private final PrintStream originalOut = System.out;
+    private ByteArrayOutputStream outputStream;
+    private PrintStream out;
 
     @Before public void
     stub_STDOUT() {
+        outputStream = new ByteArrayOutputStream();
+        out = new PrintStream(outputStream);
         System.setOut(out);
     }
 
@@ -28,7 +31,7 @@ public class GameTest {
     adds_a_new_player() {
         Game game = new Game();
         game.add("Bob");
-        assertThat(outputStream.toString(), is("Bob was added\nThey are player number 1\n"));
+        assertThat(output(), contains("Bob was added", "They are player number 1"));
     }
 
     @Test public void
@@ -39,12 +42,48 @@ public class GameTest {
         game.add("George");
         game.add("Anne");
         game.add("Timmy");
-        assertThat(outputStream.toString(), is(
-            "Julian was added\nThey are player number 1\n"
-          + "Dick was added\nThey are player number 2\n"
-          + "George was added\nThey are player number 3\n"
-          + "Anne was added\nThey are player number 4\n"
-          + "Timmy was added\nThey are player number 5\n"
+        assertThat(output(), contains(
+            "Julian was added", "They are player number 1",
+            "Dick was added", "They are player number 2",
+            "George was added", "They are player number 3",
+            "Anne was added", "They are player number 4",
+            "Timmy was added", "They are player number 5"
         ));
+    }
+
+    @Test public void
+    players_are_asked_questions() {
+        Game game = new Game();
+        game.add("Fred");
+        game.add("George");
+        game.add("Ron");
+
+        flushOutput();
+        game.roll(1);
+        game.wasCorrectlyAnswered();
+        game.roll(2);
+        game.wasCorrectlyAnswered();
+        game.roll(3);
+        game.wasCorrectlyAnswered();
+
+        assertThat(output(), contains(
+            "Fred is the current player", "They have rolled a 1",
+            "Fred's new location is 1", "The category is Science", "Science Question 0",
+            "Answer was corrent!!!!", "Fred now has 1 Gold Coins.",
+            "George is the current player", "They have rolled a 2",
+            "George's new location is 2", "The category is Sports", "Sports Question 0",
+            "Answer was corrent!!!!", "George now has 1 Gold Coins.",
+            "Ron is the current player", "They have rolled a 3",
+            "Ron's new location is 3", "The category is Rock", "Rock Question 0",
+            "Answer was corrent!!!!", "Ron now has 1 Gold Coins."
+        ));
+    }
+
+    private Iterable<String> output() {
+        return Arrays.asList(outputStream.toString().replaceFirst("\n$", "").split("\n"));
+    }
+
+    private void flushOutput() {
+        stub_STDOUT();
     }
 }
